9b68f087c1e12669086ddda6f1826261ee7f1517
Pedro Quiñones <122939370+petterquinones@users.noreply.github.com>
2026-04-15 03:17:51 +0200

    Delete example.js
    era solo un ejemplo

--- example.js
+++
@@ -1,2 +0,0 @@
-console.log("Hello desde el Ipad, esto es un ejemplo para seguir estudiando desde el ipad y complementarlo con la computadora, ya que en muchas ocasiones en muy pesado puedo hacerlo desde aca...");
-console.log("Ya el teclado no falla");
