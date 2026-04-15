5662427dd76c5d1475797a0d676efcfaf62b781a
Pedro Quiñones <122939370+petterquinones@users.noreply.github.com>
2026-04-15 03:16:40 +0200

    Add files via upload
    Ejemplo subiendo archivos desde el iPad, estoy estudiando JS y muchas veces no puedo andar con el PC por la calle, me llevare solo el iPad y en casa estudiare en el pc

---
+++ example.js
@@ -0,0 +1,2 @@
+console.log("Hello desde el Ipad, esto es un ejemplo para seguir estudiando desde el ipad y complementarlo con la computadora, ya que en muchas ocasiones en muy pesado puedo hacerlo desde aca...");
+console.log("Ya el teclado no falla");
