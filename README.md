231243434a7ae2a772c1529222fa5a1b08d300e6
Petter0915 <petter0915@gmail.com>
2026-04-17 01:51:16 +0200

    Un poco de peticiones HTTP

---
+++ README.md
@@ -0,0 +1,9 @@
+
+# IMPORTANTE TEMAS A REPASAR  
+
+    -  Objetos.
+    -  Funciones.  
+    -  CallBack y promesas.  
+    -  Consumo de http.  
+    -  Ver nuevamente la clase de http (Documento Http.js), este codigo debo leerlo y estudiarlo muy bien una ve que finalice la ruta, para evaluar lo aprendido.
+    -  
---
+++ http.js
@@ -0,0 +1,11 @@
+//** HTTP:*/
+// Es la base de las interacciones entre cliente y servidor, lo que permite a nuestars aplicaciones web funcionar de manera correcta.
+// El cliente puede ser un navegador, una pc o un dispositivo, envia solicitudes al servidor, que hospeda el contenido o servicio deseado, en respuesta el servidor provee la información o realiza acciones solicitadas.
+// Verbos HTTP:
+// GET: Se utiliza para obtener información que ya existe en el servidor sin realizar ningun cambio.
+// POST: Es empleado para enviar datos nuevos al servidor, cuando publicamos actualizaciones en alguna redSocial implementamos un post.
+// PATCH y PUT: Se utiliza para actualizar la información existente, si por ejemplo queremos corregir un error en un posyt o actualizar un perfil.
+// DELETE: Se emplea cuando deseamos eliminar un recurso del servidor.
+// http.cat
+// */
+
